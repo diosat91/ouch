71dfcf3ddf993cc8f2d68193f876f5ea2b74057b
Diosa Taylor <null>
2019-09-18 22:14:11 +0200

    edit page done

--- app/controllers/dashboard/accidents_controller.rb
+++ app/controllers/dashboard/accidents_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Dashboard::AccidentsController < ApplicationController
+  before_action :set_accident, only: [:edit, :update, :show, :destroy]
+
   def index
     @accidents = Accident.all
   end
@@ -11,15 +13,14 @@ class Dashboard::AccidentsController < ApplicationController
 
   def new
     @accident = Accident.new
-    # @accident.user = current_user
     @injury = @accident.build_injury
   end
 
   def create
     @accident = Accident.new(accident_params)
-    @accident.user = User.first
-    # @accident.injury = @injury
-    @accident.build_injury accident_params[:injury_attributes]
+    @accident.user = current_user
+    @accident.build_injury(accident_params[:injury_attributes])
+    # @accident.set_recovery_date(accident_params[:occurrence_date], accident_params[:injury_attributes][:estimated_recovery_time])
     if @accident.save!
       redirect_to dashboard_accidents_path
     else
@@ -27,16 +28,30 @@ class Dashboard::AccidentsController < ApplicationController
     end
   end
 
-  def edit; end
+  def edit
+    @injury = @accident.injury
+  end
 
-  def update; end
+  def update
+    @injury = @accident.injury
+    if @accident.update(accident_params)
+      redirect_to dashboard_accidents_path
+    else
+      render :edit
+    end
+
+  end
 
   def destroy
     @accident = Accident.find(params[:id])
     @accident.destroy
   end
 
+  def set_accident
+    @accident = Accident.find(params[:id])
+  end
+
   def accident_params
-    params.require(:accident).permit(:occurrence_date, :recovery_date, :description, :sport, injury_attributes: [:id, :body_part, :problem])
+    params.require(:accident).permit(:occurrence_date, :recovery_date, :description, :sport, injury_attributes: [:id, :body_part, :problem, :estimated_recovery_time])
   end
 end
--- app/models/accident.rb
+++ app/models/accident.rb
@@ -4,4 +4,17 @@ class Accident < ApplicationRecord
 
   accepts_nested_attributes_for :injury, allow_destroy: true
 
+  validates_presence_of :occurrence_date, :sport
+  validate :occurrence_date_not_in_future, on: :create
+  
+  # SETTERS::start
+
+  def occurrence_date_not_in_future
+    errors.add(:occurrence_date, 'cannot be in the future') if occurrence_date.to_date > Date.today
+  end
+
+  def set_recovery_date(date, number_of_days)
+    self[:recovery_date] = date + number_of_days.to_i.days
+  end
+
 end
