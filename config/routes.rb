5dac355c4d556c28c18d37fc89baaf6010440994
Diosa Taylor <null>
2020-01-23 17:14:35 +0100

    dashboard and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 	root to: "pages#home"
 
+	get 'dashboard', to: 'dashboards#show'
+
 	namespace :dashboard do
 		resources :teams
 		resources :accidents
